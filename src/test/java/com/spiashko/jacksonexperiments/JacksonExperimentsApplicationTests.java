4d91447ee2b1f2174df22670a26183b6c19e2094
Siarhei Piashko <peshko.sergey@gmail.com>
2020-10-08 13:37:42 +0300

    initial commit

---
+++ src/test/java/com/spiashko/jacksonexperiments/JacksonExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiashko.jacksonexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
